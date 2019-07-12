d063dbb4453036c1a6e6b5600c79f3f290af9589
Ikechukwu <iykekings36@gmail.com>
2019-07-12 08:54:17 +0100

    create middleware to validate post and project id

---
+++ data/middlewares/index.js
@@ -0,0 +1,44 @@
+const actionDB = require('../helpers/actionModel');
+const projectDB = require('../helpers/projectModel');
+
+const validateId = id => {
+  if (parseInt(id)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const validateProjectId = async (req, res, next) => {
+  const { id } = req.params;
+  if (validateId(id)) {
+    try {
+      const project = await projectDB.get(id);
+      console.log(project);
+      next();
+    } catch (error) {
+      res.status(500).json({ error: 'There was an error validating the id' });
+    }
+  } else {
+    res.status(400).json({ message: 'The id is invalid' });
+  }
+};
+
+const validateActionId = async (req, res, next) => {
+  const { id } = req.params;
+  if (validateId(id)) {
+    try {
+      const project = await actionDB.get(id);
+      console.log(project);
+      next();
+    } catch (error) {
+      res.status(500).json({ error: 'There was an error validating the id' });
+    }
+  } else {
+    res.status(400).json({ message: 'The id is invalid' });
+  }
+};
+module.exports = {
+  validateActionId,
+  validateProjectId
+};
